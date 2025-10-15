6820fafab82f6cb10a9c218e5ab0d12e8c4afab4
Rajashekaar <rajashekar8618350545@gmail.com>
2025-10-15 14:25:02 +0530

    first commit

---
+++ Diagnostic Management/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import doctorImage from './assets/adobe.png';
+
+
+function App() {
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <div>
+        <h1>Get Diagnostic <br />Tests & Health <br /> Checkups Done</h1>
+
+        <p>Book a hassle- free lab visit or home pickup <br />for samples and receive reports online</p>
+
+        <button className="btn btn-primary" type="button" style={{ marginLeft: '250px' }}>Book Now</button>
+
+      </div>
+       <div>
+        <img src={doctorImage} alt="Doctor" style={{ width: '400px', height: '400px' }} />
+       </div>
+    </div>
+  );
+}
+
+export default App;
